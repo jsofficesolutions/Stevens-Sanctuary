8188ec91b4ba6fe41a99bbba6c41e950530fb03a
jsofficesolutions <jsofficesolutionsltd@gmail.com>
2026-06-02 22:46:47 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,4 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
+const firebaseConfig = typeof __firebase_config !== 'undefined' ? JSON.parse(__firebase_config) : {
   apiKey: "AIzaSyBzarUX4dkMs487-doUfG1Ct8oivtoLX9Y",
   authDomain: "stevens-sanctuary.firebaseapp.com",
   projectId: "stevens-sanctuary",
@@ -10,8 +6,7 @@ const firebaseConfig = {
   messagingSenderId: "45718875766",
   appId: "1:45718875766:web:f06156090035d44366b957"
 };
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const appId = "stevens-sanctuary";
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const appId = typeof __app_id !== 'undefined' ? __app_id : "stevens-sanctuary";
